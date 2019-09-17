f62be31fc5db102b413fb45c81477e340626e765
suvro.me <suvrodatta95@gmail.com>
2019-09-17 15:18:28 +0600

    ui-changing-logs-and-notification-component

--- src/Components/Logs.vue
+++ src/Components/Logs.vue
@@ -1,9 +1,62 @@
 <template>
     <div>
-        <h1>Hello From Logs</h1>
-        <pre>{{logs}}</pre>
+        <el-row>
+            <el-form>
+                <el-col :span="4">
+                    <el-form-item>
+                        <el-select v-model="value" placeholder="Type">
+                            <el-option v-for="item in logs" :key= "item.log_type" :label= "item.log_type" :value= "item.log_type">
+                            </el-option>
+                        </el-select>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="4">
+                    <el-form-item>
+                        <el-input placeholder="Search in logs" prefix-icon="el-icon-search"></el-input>
+                    </el-form-item>
+                </el-col>
+            </el-form>
+            <el-col>
+                <el-table :data="logs" border style="width: 100%">
+                    <el-table-column type="index" label="Id" width="80">
+                        
+                    </el-table-column>
+
+                    <el-table-column prop="log_data" label="log data" width="500">
+
+                    </el-table-column>
+
+                    <el-table-column prop="request_method" label="request method">
+
+                    </el-table-column>
+
+                    <el-table-column  prop="created_at" label="created at">
+
+                    </el-table-column>
+
+                    <el-table-column prop="log_type" label="type">
+
+                    </el-table-column>
+                </el-table>
+
+                <div class="pagination-section">
+                    <el-pagination background layout="prev, pager, next" :total="total_logs">
+                    </el-pagination>
+                </div>
+            </el-col>
+        </el-row>
+        <!-- <pre>{{logs}}</pre> -->
     </div>
 </template>
+
+<style>
+
+    .pagination-section{
+        margin-top:20px;
+        float: right;
+    }
+
+</style>
 
 <script type="text/babel">
     export default {
@@ -15,7 +68,12 @@
                 per_page: 20,
                 page: 1,
                 search: '',
-                fetching: false
+                fetching: false,
+                formInline: {
+                    user: '',
+                    region: ''
+                },
+                value: ''               
             }
         },
         methods: {
--- src/Components/NotificationSettings.vue
+++ src/Components/NotificationSettings.vue
@@ -1,11 +1,86 @@
 <template>
     <div>
-        <h1>Hello From Log Notifications</h1>
+        <el-container>
+            <el-row class="email-section">
+                <el-col>
+                    <h3> Email Notification Settings :</h3>
+                </el-col>
+                <el-col>
+                    <label>Email to :</label>
+                    <input type="text" Placeholder="Email" />
+                </el-col>
+            </el-row>
+        </el-container>
+
+        <el-container>
+            <el-row class="notification-section">
+                <el-col>
+                    <h3>Notification Types Settings :</h3>
+                </el-col>
+
+                <el-col>
+                    <ul class="myclass">
+                        <li v-for="name in error_names" :label="name" :key="name"><input type="checkbox"><span> {{name}}</span></li>
+                    </ul>
+                </el-col>
+            </el-row>
+        </el-container>
+
+        <el-container>
+            <el-row class="database-log-section">
+                <el-col>
+                    <h3>Database Logs Settings :</h3>
+                </el-col>
+                <el-col>
+                    <ul class="myclass">
+                        <li v-for="name in error_names" :label="name" :key="name"><input type="checkbox"><span> {{name}}</span></li>
+                    </ul>
+                </el-col>
+            </el-row>
+        </el-container>
+
+        <el-row>
+            <div class="btn-save">
+                <el-button type="success" size="mini">Save</el-button>
+            </div>
+        </el-row>
+           
     </div>
+
 </template>
 
 <script type="text/babel">
     export default {
-        name: 'error_log_notifications'
+        name: 'error_log_notifications',
+        data() {
+            return {
+                checkList1: [],
+                checkList2: ['selected and disabled', 'Option A'],
+                error_names: [
+                    'E_ERROR', 'E_WARNING', 'E_PARSE', 'E_NOTICE', 'E_CORE_ERROR', 'E_CORE_WARNING', 'E_COMPILE_ERROR', 'E_COMPILE_WARNING', 'E_USER_ERROR', 'E_USER_WARNING', 'E_USER_NOTICE', 'E_STRICT', 'E_RECOVERABLE_ERROR', 'E_DEPRECATED', 'E_USER_DEPRECATED'
+                ]
+            };
+        }
     }
 </script>
+
+<style typle="text/css">
+
+    .email-section{
+        margin-top: 15px;
+        margin-bottom:15px;
+    }
+
+    .notification-section{
+        margin-bottom:15px;
+    }
+
+    .database-log-section{
+        margin-bottom:15px;
+    }
+
+    .btn-save{
+        margin-top: 15px;
+    }
+
+</style>
